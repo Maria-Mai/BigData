9a8f64dc89b1f49aaf0dc4b11f2c312366049c16
Maria-Mai <mai.blo99er@gmail.com>
2019-01-16 13:57:11 +0100

    versuch von map mit gruppen

--- mapWithGroups.py
+++ mapWithGroups.py
@@ -1,7 +1,7 @@
 from itertools import combinations
 
 formattedHarry = open("formattedHarry.txt", mode="r", encoding="UTF-8").read()
-formattedHarry = formattedHarry.split(".")
+formattedHarry = formattedHarry.split(".") #6137 sätze #TODO  mr. mrs. entfernen und !? hinzufügen
 
 characterList = ["Harry", "Ron", "Hermione", "Ginny", "Neville", "Luna",
                  "James", "Lily", "Malfoy", "Hagrid", "Dumbledore", "Voldemort",
@@ -9,20 +9,22 @@ characterList = ["Harry", "Ron", "Hermione", "Ginny", "Neville", "Luna",
                  "Arthur", "Molly", "Bill", "Fleur", "Horace", "Umbridge"]
 
 characterList = list(map(lambda x:x.lower(), characterList))
+allCharacterCombinations = list(combinations(characterList,2))
 
-# Schlüsselpaar [i,j] wird ersetzt durch [I,J]. Teile n Charaktere in g Gruppen
-# mit je n/g Charakterpaaren.
-character_pairs = list(combinations(characterList, 2))
+g = 1000
+n = len(formattedHarry) # n = anzahl (6137) sätze
 
-n = len(character_pairs) # 300
-g = 5           
-print(n/g)  # 60
-for I in range(g):  
-    for J in range(g):
-        if (I is not J):
-            print((I, J), (character_pairs[I]))
-        else:
-            continue
+I = int(n/g) #anzahl gruppen (gerade 5)
+J = int(n/g) #anzahl gruppen (gerade 5)
 
+# keine Ahnung ob das so richtig ist, aber besser bekomm ich es nicht hin
 
-
+#für alle sätze
+for sentence in formattedHarry:
+    #alle combos von I und J
+    for i in range(I):
+        for j in range(J):
+            #wenn paar in satz, dann ausgeben
+            for k in range(len(allCharacterCombinations)-1):
+                if((i != j) and (allCharacterCombinations[k][0] in sentence) and (allCharacterCombinations[k][1] in sentence)):
+                    print((i,j), (sentence, allCharacterCombinations[k]))
