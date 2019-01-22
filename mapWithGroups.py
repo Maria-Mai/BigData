594c0e1e8cc6f659fa8f938e0af8dd07e9235b44
flys1ck <34650380+flys1ck@users.noreply.github.com>
2019-01-22 09:59:03 +0100

    removed dictionary, fixed grouping

--- mapWithGroups.py
+++ mapWithGroups.py
@@ -1,18 +1,19 @@
 from itertools import combinations
 
 formattedHarry = open("formattedHarry.txt", mode="r", encoding="UTF-8").read()
-formattedHarry = formattedHarry.split(".") #6137 sätze #TODO  mr. mrs. entfernen und !? hinzufügen
+# 6137 sätze #TODO  mr. mrs. entfernen und !? hinzufügen
+formattedHarry = formattedHarry.split(".")
 
 characterList = ["Harry", "Ron", "Hermione", "Ginny", "Neville", "Luna",
                  "James", "Lily", "Malfoy", "Hagrid", "Dumbledore", "Voldemort",
                  "Sirius", "Remus", "Bellatrix", "Snape", "McGonagall", "Fred", "George",
                  "Arthur", "Molly", "Bill", "Fleur", "Horace", "Umbridge"]
 
-characterList = list(map(lambda x:x.lower(), characterList))
-allCharacterCombinations = list(combinations(characterList,2))
+characterList = list(map(lambda x: x.lower(), characterList))
+allCharacterCombinations = list(combinations(characterList, 2))
 
 # g = 1000
-n = len(formattedHarry) # n = anzahl (6137) sätze
+n = len(formattedHarry)  # n = anzahl (6137) sätze
 
 # I = int(n/g) #anzahl gruppen (gerade 5)
 # J = int(n/g) #anzahl gruppen (gerade 5)
@@ -28,24 +29,17 @@ n = len(formattedHarry) # n = anzahl (6137) sätze
 #             for k in range(len(allCharacterCombinations)-1):
 #                 if((i != j) and (allCharacterCombinations[k][0] in sentence) and (allCharacterCombinations[k][1] in sentence)):
 #                     print((i, j), (sentence, allCharacterCombinations[k]))
-                    
+
 # Wir teilen unseren Datensatz (Bsp: Harry Potter - Stein der Weisen) mit n Sätzen (6137) in g Gruppen.
-g = 5 # Anzahl der zur Verfügung stehenden Rechner
+g = 5  # Anzahl der zur Verfügung stehenden Rechner
 
-characterDictionary = {}
-
-for characterCombination in allCharacterCombinations:
-    characterDictionary[characterCombination] = 0
-
-# print(characterDictionary)
-# Laufvariable mit if(): break
-
+start = 0
 for J in range(g):
-    for sentence in formattedHarry[ J*(int(n/g)) : (J+1)*int(n/g)-1 ]: # hier nochmal überprüfen
+    end = (J+1)*int(n/g) + J
+    if (end > n):
+        end = n-1
+    for sentence in formattedHarry[start: end]:
         for characterCombination in allCharacterCombinations:
             if (characterCombination[0] in sentence and characterCombination[1] in sentence):
-                characterDictionary[characterCombination] += 1
-    print("mapper / group:", J)
-    print(characterDictionary)
-
-print(characterDictionary)
+                print(characterCombination[0], characterCombination[1], 1)
+    start = end + 1
